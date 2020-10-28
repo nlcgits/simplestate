c47dc6204428ca82f98d252646cce76bbb9cf51f
Joseph Cohen <57691141+nlcgits@users.noreply.github.com>
2020-10-27 20:04:37 -0400

    Update SimpleState.js

--- src/SimpleState.js
+++ src/SimpleState.js
@@ -218,7 +218,7 @@ class SimpleState {
       return () => {
         state.subscribers.delete(subscriber);
       };
-    });
+    }, []);
     return [value, (val) => this.setSingleState(name, val)];
   }
 }
